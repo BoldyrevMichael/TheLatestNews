408a312af0982e0a64a1b7f6591ab1215a972edf
BoldyrevMichael <mmmsnddd@icloud.com>
2018-08-09 15:37:38 +0300

    1. Добавлено логирование методов контроллеров с помощью АОП (класс LogAspect). 2. Продолжена разработка html-страниц и их контроллеров.

---
+++ src/main/java/ru/boldyrev/ma/spring2/thelatestnews/log/LogAspect.java
@@ -0,0 +1,24 @@
+package ru.boldyrev.ma.spring2.thelatestnews.log;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+
+    // определяем срез по всем методам бинов из пакета com.example.aop.service
+    @Pointcut("execution(* ru.boldyrev.ma.spring2.thelatestnews.controller..*.*(..))")
+    private void getName() {
+    }
+
+    // определяем совет (Advice) "ПЕРЕД" выполнением кода бина (класса)
+    @Before("getName()")
+    public void logBefore(JoinPoint joinPoint) {
+        // выводим в консоль информацию о текущей точке соединения
+        System.out.println(joinPoint);
+    }
+}
